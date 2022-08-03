eea7bcad5a76c7e08badd1a11986521229daaeee
kpermenter <52431178+kpermenter@users.noreply.github.com>
2022-08-03 15:52:14 -0500

    Delete client directory

--- client/src/context/ExpenseContextContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { ExpenseLogsProvider } from './ExpenseContext';
-
-export const ExpenseLogsContextContainer = ({ children }: React.PropsWithChildren<{}>) => {
-  return (
-      <ExpenseLogsProvider>
-        { children }
-      </ExpenseLogsProvider>
-  );
-};
